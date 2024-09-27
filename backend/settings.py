4eba97ae4a416688f2138d0933fd2184d87f0fa4
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-09-27 17:42:19 -0500

    csrf

--- backend/settings.py
+++ backend/settings.py
@@ -133,3 +133,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = ["http://*"]
