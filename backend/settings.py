0fee3f282230776a3b4063ad18b51b07f654c763
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-09-27 17:39:27 -0500

    whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoisee.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'corsheaders',
     'rest_framework',
     'api',
