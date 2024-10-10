e51a0f2564ffd25097b401868141cf2e6f41d1f1
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-10-09 22:19:56 -0500

    .

--- api/admin.py
+++ api/admin.py
@@ -14,11 +14,14 @@ class TipoDispositivoAdmin(admin.ModelAdmin):
 class DispositivoAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Dispositivo._meta.fields]
 
+class MedidasHabitacionAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in MedidasHabitacion._meta.fields]
+
 # Register your models here.
 admin.site.register(Casa, CasaAdmin)
 admin.site.register(Habitacion, HabitacionAdmin)
 admin.site.register(TipoDispositivo, TipoDispositivoAdmin)
 admin.site.register(Dispositivo, DispositivoAdmin)
-admin.site.register(MedidasHabitacion)
+admin.site.register(MedidasHabitacion, MedidasHabitacionAdmin)
 admin.site.register(EnergiaDispositivo)
 admin.site.register(ProgramacionDispositivo)
